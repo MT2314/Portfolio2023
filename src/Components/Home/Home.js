7aba49e9939a38d391dfd993eb1c8ae80a5192e1
Mike Tkachuk <tkachuk2314@gmail.com>
2023-01-19 08:59:49 -0500

    button fix

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -14,52 +14,50 @@ export const Home = () => {
   const [modalShow, setModalShow] = useState(false);
   return (
     <>
-      <div className="page">
-        <ContactPage show={modalShow} onHide={() => setModalShow(false)} />
+      <ContactPage show={modalShow} onHide={() => setModalShow(false)} />
 
-        {!modalShow && (
-          <div className="hero">
-            <h1 className="hero-text">
-              Hi, I am
-              <span> Mike Tkachuk.</span>
-            </h1>
-            <p className="h-sub-text">
-              Practiced Developer capable of building web applications from the
-              ground up. Proven experience developing consumer-focused websites
-              using modern technologies.
-            </p>
-            <div className="icons">
-              <a href={resume} className="icon-holder" target="_blank">
-                <ImLibreoffice
-                  className="icon resume"
-                  style={{ height: "80%" }}
-                />
-              </a>
-              <a
-                href="https://github.com/MT2314"
-                className="icon-holder"
-                target="_blank"
-              >
-                <AiFillGithub className="icon gh" />
-              </a>
-              <a
-                href="https://linkedin.com/in/mike-tkachuk-82909111a"
-                className="icon-holder"
-                target="_blank"
-              >
-                <AiFillLinkedin className="icon ln" />
-              </a>
-            </div>
-            <Button
-              className="ContactForm"
-              variant="primary"
-              onClick={() => setModalShow(true)}
+      {!modalShow && (
+        <div className="hero">
+          <h1 className="hero-text">
+            Hi, I am
+            <span> Mike Tkachuk.</span>
+          </h1>
+          <p className="h-sub-text">
+            Practiced Developer capable of building web applications from the
+            ground up. Proven experience developing consumer-focused websites
+            using modern technologies.
+          </p>
+          <div className="icons">
+            <a href={resume} className="icon-holder" target="_blank">
+              <ImLibreoffice
+                className="icon resume"
+                style={{ height: "80%" }}
+              />
+            </a>
+            <a
+              href="https://github.com/MT2314"
+              className="icon-holder"
+              target="_blank"
             >
-              Contact Me
-            </Button>
+              <AiFillGithub className="icon gh" />
+            </a>
+            <a
+              href="https://linkedin.com/in/mike-tkachuk-82909111a"
+              className="icon-holder"
+              target="_blank"
+            >
+              <AiFillLinkedin className="icon ln" />
+            </a>
           </div>
-        )}
-      </div>
+          <Button
+            className="ContactForm"
+            variant="primary"
+            onClick={() => setModalShow(true)}
+          >
+            Contact Me
+          </Button>
+        </div>
+      )}
     </>
   );
 };
